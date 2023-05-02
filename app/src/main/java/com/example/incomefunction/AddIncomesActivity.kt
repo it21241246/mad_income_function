96b459fedf91ae6064731936d96682bfc0c56624
it21241246 <it21241246@my.sliit.lk>
2023-05-02 16:36:46 +0530

    initial commit with input feature

---
+++ app/src/main/java/com/example/incomefunction/AddIncomesActivity.kt
@@ -0,0 +1,100 @@
+package com.example.incomefunction
+
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.FirebaseFirestore
+import java.text.SimpleDateFormat
+import java.util.*
+
+class AddIncomesActivity : AppCompatActivity() {
+
+    private lateinit var incomeNameEditText: EditText
+    private lateinit var incomeAmountEditText: EditText
+    private lateinit var dateEditText: EditText
+    private lateinit var timeEditText: EditText
+    private lateinit var saveIncomeButton: Button
+
+    private val db = FirebaseFirestore.getInstance()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_incomes)
+
+        incomeNameEditText = findViewById(R.id.incomeNameInput)
+        incomeAmountEditText = findViewById(R.id.incomeAmountInput)
+        dateEditText = findViewById(R.id.incomeDateInput)
+        timeEditText = findViewById(R.id.incomeTimeInput)
+        saveIncomeButton = findViewById(R.id.saveIncomeButton)
+
+        // Get the current date and time
+        val calendar = Calendar.getInstance()
+        val dateFormat = SimpleDateFormat("MMM dd, yyyy", Locale.getDefault())
+        val timeFormat = SimpleDateFormat("h:mm a", Locale.getDefault())
+        dateEditText.setText(dateFormat.format(calendar.time))
+        timeEditText.setText(timeFormat.format(calendar.time))
+
+        // Set up click listeners for the date and time fields to allow the user to edit them
+        dateEditText.setOnClickListener {
+            val year = calendar.get(Calendar.YEAR)
+            val month = calendar.get(Calendar.MONTH)
+            val dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH)
+            val datePickerDialog = DatePickerDialog(this,
+                { _, selectedYear, selectedMonth, selectedDayOfMonth ->
+                    calendar.set(selectedYear, selectedMonth, selectedDayOfMonth)
+                    dateEditText.setText(dateFormat.format(calendar.time))
+                },
+                year,
+                month,
+                dayOfMonth
+            )
+            datePickerDialog.show()
+        }
+
+        timeEditText.setOnClickListener {
+            val hour = calendar.get(Calendar.HOUR_OF_DAY)
+            val minute = calendar.get(Calendar.MINUTE)
+            val timePickerDialog = TimePickerDialog(this,
+                { _, selectedHour, selectedMinute ->
+                    calendar.set(Calendar.HOUR_OF_DAY, selectedHour)
+                    calendar.set(Calendar.MINUTE, selectedMinute)
+                    timeEditText.setText(timeFormat.format(calendar.time))
+                },
+                hour,
+                minute,
+                false
+            )
+            timePickerDialog.show()
+        }
+
+        saveIncomeButton.setOnClickListener {
+            val incomeName = incomeNameEditText.text.toString().trim()
+            val incomeAmount = incomeAmountEditText.text.toString().toDouble()
+            val date = calendar.time
+
+            // Create a new income document with the data and date
+            val income = hashMapOf(
+                "name" to incomeName,
+                "amount" to incomeAmount,
+                "date" to date
+            )
+
+            // Add the income to the 'incomes' collection in Firestore
+            db.collection("incomes")
+                .add(income)
+                .addOnSuccessListener { documentReference ->
+                    // Show a success message and finish the activity
+                    Toast.makeText(this, "Income added successfully", Toast.LENGTH_SHORT).show()
+                    finish()
+                }
+                .addOnFailureListener { e ->
+                    // Show an error message if the income couldn't be added
+                    Toast.makeText(this, "Error adding income: ${e.message}", Toast.LENGTH_SHORT).show()
+                }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/incomefunction/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.incomefunction
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        val viewincomes: Button = findViewById(R.id.dashboardButton)
+//        viewincomes.setOnClickListener {
+//            val intent = Intent(this, DisplayIncomeActivity::class.java)
+//            startActivity(intent)
+//        }
+
+
+//        val addExpenseButton: Button = findViewById(R.id.addExpenseButton)
+//        addExpenseButton.setOnClickListener {
+//            val intent = Intent(this, AddExpensesActivity::class.java)
+//            startActivity(intent)
+//        }
+
+        val addIncomeButton: Button = findViewById(R.id.addIncomeButton)
+        addIncomeButton.setOnClickListener {
+            val intent = Intent(this, AddIncomesActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
